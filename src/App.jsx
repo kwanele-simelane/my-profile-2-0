3b1d4bed0d777714ba684f1c3080f883d476ae88
Kwanele Simelane <83864710+kwanele-simelane@users.noreply.github.com>
2025-11-08 19:21:42 +0200

    Update App.jsx
    Update google tags to track website traffic

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Home from "./views/Home";
 import Archive from "./views/Archive";
 import NotFound from "./views/NotFound";
 
-const App = () => {
+// Track route changes
+const RouteChangeTracker = () => {
+  const location = useLocation();
+
   useEffect(() => {
     ReactGA.send({
       hitType: "pageview",
-      page: window.location.pathname,
+      page: location.pathname + location.search,
     });
-  }, []);
+  }, [location]);
+
+  return null;
+};
+
+const App = () => {
+  ReactGA.initialize("G-DK2HG29J21");
 
   return (
     <Router>
+      <RouteChangeTracker />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/archive" element={<Archive />} />
